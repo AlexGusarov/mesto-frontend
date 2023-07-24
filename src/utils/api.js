a527770a8b2f2094d269225be46a60246630acef
Alex Gus <radiogusarov@gmail.com>
2023-07-24 13:01:44 +0300

    initial commit

---
+++ src/utils/api.js
@@ -0,0 +1,127 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this.headers = headers;
+  }
+
+  _handleResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  _request(url, options) {
+    return fetch(url, options).then(this._handleResponse)
+  }
+
+  getInitialCards() {
+    //если убрать токен, то после входа карточки появятся только после перезагрузки
+    const token = localStorage.getItem('token');
+    return this._request(`${this._baseUrl}/cards`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+
+  getUserInfo() {
+    // если убрать токен, то при входе новым пользователем будут видны данные предыдущего 
+    const token = localStorage.getItem('token');
+    return this._request(`${this._baseUrl}/users/me`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  editProfile({ name, about }) {
+    //если убрать токен, то функция не работает до обновления страницы
+    const token = localStorage.getItem('token');
+    return this._request(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: {
+        authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, about })
+    })
+  }
+
+  getNewCard({ name, link }) {
+    //если убрать токен, то функция не работает до обновления страницы
+    const token = localStorage.getItem('token');
+    return this._request(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: {
+        authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        link: link
+      })
+    })
+  }
+
+  deleteCard(cardId) {
+    return this._request(`${this._baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this.headers
+    })
+  }
+
+  putLike(cardId) {
+    const token = localStorage.getItem('token');
+    return this._request(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: 'PUT',
+      headers: {
+        authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+    })
+  }
+
+  deleteLike(cardId) {
+    const token = localStorage.getItem('token');
+    return this._request(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: 'DELETE',
+      headers: {
+        authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  changeLikeCardStatus(cardId, isLiked) {
+    return isLiked ? this.putLike(cardId) : this.deleteLike(cardId);
+  }
+
+  editAvatar(data) {
+    const token = localStorage.getItem('token');
+    return this._request(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: {
+        authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        avatar: data.avatar
+      })
+    })
+  }
+}
+
+const api = new Api({
+  baseUrl: 'https://mesto-server-1bb72610626b.herokuapp.com',
+  // baseUrl: 'http://localhost:3000',
+  headers: {
+    authorization: `Bearer ${localStorage.getItem('token')}`,
+    'Content-Type': 'application/json'
+  }
+});
+
+export default api;
